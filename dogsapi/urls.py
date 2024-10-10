dee75f5db4f1c257e33e4d83c0f8c5fb6aef0b46
aidarminyazev <legodark@mail.ru>
2024-10-11 01:11:44 +0500

    api urls added

--- dogsapi/urls.py
+++ dogsapi/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .controllers import DogList, DogDetail, BreedViewSet
 from . import views
 
+router = DefaultRouter()
+router.register(r'breeds', BreedViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include(router.urls)),
     path('about/', views.about, name='about'),
+    path('dogs/', DogList.as_view(), name='dog-list'),
+    path('dogs/<int:pk>/', DogDetail.as_view(), name='dog-detail'),
 ]
